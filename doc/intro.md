c49a6e93cfd0f7c73f892dafb5927b9a1b440efa
Anthony Grimes <i@raynes.me>
2013-02-08 14:51:56 -0800

    First commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to turntable
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/turntable/core_test.clj
@@ -0,0 +1,7 @@
+(ns turntable.core-test
+  (:use clojure.test
+        turntable.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
