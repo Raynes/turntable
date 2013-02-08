5136b6fb9f8c7a48164baff4b1fae541479986f7
Anthony Grimes <i@raynes.me>
2013-02-08 14:53:06 -0800

    Use the flatland naming scheme.

--- src/turntable/core.clj
+++
@@ -1,6 +0,0 @@
-(ns turntable.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/turntable/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns turntable.core-test
-  (:use clojure.test
-        turntable.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
