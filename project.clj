443f13a459c91f5dc27d74d3571937abae149930
Anthony Grimes <i@raynes.me>
2013-02-08 15:02:50 -0800

    Add some info to project.clj and config.clj (where queries will be persisted to .gitignore).

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject turntable "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A service for running SQL queries every n minutes."
+  :url "https://github.com/flatland/turntable"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.5"]
+                 [cheshire "5.0.1"]
+                 [ring-middleware-format "0.2.4"]])
