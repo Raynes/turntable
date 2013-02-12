449be5ebd5e7ca298d7b32df2563b822a1e396e3
Anthony Grimes <i@raynes.me>
2013-02-11 17:33:33 -0800

    First draft of add-query and query scheduling mechanism.

--- project.clj
+++ project.clj
@@ -6,4 +6,8 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.5"]
                  [cheshire "5.0.1"]
-                 [ring-middleware-format "0.2.4"]])
+                 [ring-middleware-format "0.2.4"]
+                 [overtone/at-at "1.1.1"]
+                 [clj-time "0.4.4"]]
+  :plugins [[lein-ring "0.8.2"]]
+  :ring {:handler flatland.turntable/handler})
--- src/flatland/turntable.clj
+++ src/flatland/turntable.clj
@@ -1,1 +1,98 @@
-(ns flatland.turntable)
+(ns flatland.turntable
+  (:require [compojure.core :refer [GET POST ANY defroutes routes]]
+            (ring.middleware [format-params :refer :all]
+                             [params :refer :all]
+                             [keyword-params :refer :all])
+            [clj-time.core :refer [in-minutes now interval]]
+            [overtone.at-at :refer [mk-pool every stop]]))
+
+(def ^:const minute
+  "One minute in millseconds."
+  60000)
+
+(def pool
+  "A thread pool for usage with at-at."
+  (mk-pool))
+
+(defn mins
+  "Minutes to milliseconds."
+  [m]
+  (* m minute))
+
+(def running
+  "Queries that are currently running. It is a hash of the names associated
+   with the queries to a map containing the query, the interval between runs,
+   and the results from the last run."
+  (atom {}))
+
+(def cooldowns
+  "A map of intervals to cooldowns."
+  (atom {}))
+
+(defn cooldown
+  "Start a cooldown of 1 minute on this interval."
+  [minutes]
+  (swap! cooldowns assoc minutes (promise))
+  (future
+    (Thread/sleep (mins 1))
+    (swap! cooldowns update-in [minutes] deliver true)))
+
+(defn await-cooldown
+  "Check for a cooldown in place on this interval and if present, wait for
+   the promise to be delivered (the cooldown ending)."
+  [minutes]
+  (if-let [cd (@cooldowns (min minutes))]
+    @cd
+    true))
+
+(defn query-fn
+  "Returns a function that runs a query, records start and end time,
+   and updates running with the results and times when finished."
+  [name query]
+  (fn []
+    (let [start (now)
+          results (println "This will eventually do stuff.")
+          stop (now)]
+      (swap! running update-in [name] assoc
+             :results results
+             :start start
+             :stop stop
+             :elapsed (in-minutes (interval start stop))))))
+
+(defn add-query
+  "Schedule a query to run at most once at minutes intervals. Adheres to a
+   one minute cooldown between scheduling queries to help prevent too much
+   database load."
+  [name query minutes]
+  (let [minutes (mins minutes)]
+    (when (await-cooldown minutes)
+      (swap! running
+             assoc name {:query query
+                         :minutes minutes
+                         :scheduled-fn (every minutes
+                                              (query-fn name query)
+                                              pool)}))))
+
+(defn remove-query [name])
+
+(defn get-query [name])
+
+(defn list-queries [])
+
+(defroutes writers
+  (POST "/add" [name query minutes]
+    (add-query name query minutes))
+  (POST "/remove" [name]
+    (remove-query name)))
+
+(defroutes readers
+  (ANY "/get" [name]
+    (get-query name))
+  (GET "/list" []
+    (list-queries)))
+
+(def handler
+  (-> (routes readers writers)
+      (wrap-keyword-params)
+      (wrap-params)
+      (wrap-json-params)))
