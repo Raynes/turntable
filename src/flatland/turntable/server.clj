85bdc2ffc36feba51793f75fa900cc711e772084
Anthony Grimes <i@raynes.me>
2013-03-11 17:48:43 -0700

    Read from new sample config for base server and pass config to routes (stoopid).

--- src/flatland/turntable/server.clj
+++ src/flatland/turntable/server.clj
@@ -1,8 +1,8 @@
 (ns flatland.turntable.server
   (:require [flatland.turntable.service :refer [turntable-routes init-saved-queries]]))
 
-(def config {:query-file "queries.clj"})
+(def config (read-string (slurp "sample.config.clj")))
 
 (init-saved-queries config)
 
-(def handler turntable-routes)
+(def handler (turntable-routes config))
