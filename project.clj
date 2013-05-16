887739ff44283e47a1d6a342742521768a85156c
Anthony Grimes <i@raynes.me>
2013-05-15 17:04:21 -0700

    Inc version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/turntable "0.1.8"
+(defproject org.flatland/turntable "0.1.9"
   :description "A service for running SQL queries every n minutes."
   :url "https://github.com/flatland/turntable"
   :license {:name "Eclipse Public License"
