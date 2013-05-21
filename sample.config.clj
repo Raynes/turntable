2e8b886b8b518e3402b6e29dc90ce5e783150fa9
Anthony Grimes <i@raynes.me>
2013-05-20 18:01:42 -0700

    Return seconds instead of milliseconds in /render.

--- sample.config.clj
+++ sample.config.clj
@@ -1,2 +1,5 @@
-{:servers {"127.0.0.1:5432/foo" {:subprotocol "postgresql"}}
+{:servers {"db3" {:subprotocol "postgresql"
+		  :subname "//db3.geni.com:5432/wg_production"
+	          :user "agrimes"
+		  :password "WorldInChaos0"}}
  :query-file "queries.clj"}
